ca9e99ab086bc16ab8764491c27beef1b5d1f133
satvikjadhav <satvikj5@gmail.com>
2025-04-07 23:14:34 -0500

    Initial Commit

---
+++ PixelPal/PixelPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelPalApp.swift
+//  PixelPal
+//
+//  Created by Satvik  Jadhav on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PixelPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
